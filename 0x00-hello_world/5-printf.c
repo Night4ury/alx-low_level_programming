730b5416f52136ecc8626ab73460592d92a18269
Night4ury <a.waleedelzayat@gmail.com>
2023-07-16 04:56:47 -0700

    Adding

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main - Start of the main fucntions
+ *
+ * Description: Programm that prints 1 line
+ *
+ * Return: nothing
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
