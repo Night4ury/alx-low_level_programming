0ce78061a1c50fb6d420d498d1fac34325a903e2
Night4ury <a.waleedelzayat@gmail.com>
2023-07-16 07:52:48 -0700

    Adding

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - Start of the main fucntions
+ *
+ * Description: Programm that Size of diffrent data types
+ *
+ * Return: nothing
+ */
+int main(void)
+{
+printf("Size of char: %zu Byte(s)",sizeof(char));
+printf("Size of int: %zu Byte(s)",sizeof(int));
+printf("Size of long int: %zu Byte(s)",sizeof(long int));
+printf("Size of long long int: %zu Byte(s)",sizeof(long long int));
+printf("Size of float: %zu Byte(s)",sizeof(float));
+return (0);
+}
