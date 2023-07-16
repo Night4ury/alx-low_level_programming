de6174d28f320002c068e2bb17117cbc14e05a5d
Night4ury <a.waleedelzayat@gmail.com>
2023-07-16 08:14:04 -0700

    Adding

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - Start of the main fucntions
+ *
+ * Description: Programm that Size of diffrent data types
+ *
+ * Return: nothing
+ */
+int main(void)
+{
+
+char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+for(int i=0; str[i]!= '\0'; i++)
+{
+putchar(i);
+}
+return (1);
+}
