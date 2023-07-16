3c8356df35c0f85ad41516bbbd36e3e9fbc1b070
Night4ury <a.waleedelzayat@gmail.com>
2023-07-16 07:54:05 -0700

    Adding

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,10 +8,10 @@
  */
 int main(void)
 {
-printf("Size of char: %zu Byte(s)",sizeof(char));
-printf("Size of int: %zu Byte(s)",sizeof(int));
-printf("Size of long int: %zu Byte(s)",sizeof(long int));
-printf("Size of long long int: %zu Byte(s)",sizeof(long long int));
-printf("Size of float: %zu Byte(s)",sizeof(float));
+printf("Size of char: %zu Byte(s)", sizeof(char));
+printf("Size of int: %zu Byte(s)", sizeof(int));
+printf("Size of long int: %zu Byte(s)", sizeof(long int));
+printf("Size of long long int: %zu Byte(s)", sizeof(long long int));
+printf("Size of float: %zu Byte(s)", sizeof(float));
 return (0);
 }
