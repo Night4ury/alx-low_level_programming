a483666726ddddac324d0b1543bcb16542100cc9
Night4ury <a.waleedelzayat@gmail.com>
2023-07-16 04:43:48 -0700

    Adding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include<stdio.h>
-
-int main (){
-
-    puts("\"Programming is like building a multilingual puzzle");
-    return 0;
+/*
+ * main - Start of the main fucntions
+ *
+ * Return: nothing
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
