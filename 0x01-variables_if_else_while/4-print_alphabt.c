ae373b06972601d789f075edb0f0470ab4dd5c3d
Night4ury <a.waleedelzayat@gmail.com>
2023-07-17 07:56:55 -0700

    Adding

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Start of the main fucntions
+ *
+ * Description: Programm that prints the alphabet in lowercase & uppercase.
+ *
+ * Return: nothing
+ */
+int main(void)
+{
+printf("abcdfghijklmnoprstuvwxyz\n");
+return (0);
+}
