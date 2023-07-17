f9057b1ad59443046eac93cac32d8ca018723586
Night4ury <a.waleedelzayat@gmail.com>
2023-07-17 08:45:24 -0700

    Adding

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Start of the main fucntions
+ *
+ * Description: Programm that prints the alphabet in lowercase & uppercase.
+ *
+ * Return: nothing
+ */
+int main(void)
+{
+int num = 0;
+while (num <= 9)
+{
+putchar(num + '0');
+num++;
+}
+return (0);
+}
