a961f3aaab0667e70d57468c2517eaa35b9884d3
Night4ury <a.waleedelzayat@gmail.com>
2023-07-17 07:46:15 -0700

    Adding

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,15 +1,13 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 /**
  * main - Start of the main fucntions
  *
- * Description: Programm that prints the alphabet in lowercase, followed by a new line.
+ * Description: Programm that prints the alphabet in lowercase.
  *
  * Return: nothing
  */
 int main(void)
 {
 printf("abcdefghijklmnopqrstuvwxyz\n");
-return(0);
+return (0);
 }
