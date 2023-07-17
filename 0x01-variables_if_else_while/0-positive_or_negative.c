a9eca4fcba64a4b1b40243b942a7fb9af08c25b4
Night4ury <a.waleedelzayat@gmail.com>
2023-07-17 04:32:45 -0700

    Adding

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Start of the main fucntions
+ *
+ * Description: Programm that Size of diffrent data types
+ *
+ * Return: nothing
+ */
+int main(void)
+{
+int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+/* your code goes there */
+if (n > 0)
+printf("%d is positive", n);
+else if (n == 0)
+printf("%d is zero", n);
+else
+printf("%d is negative", n);
+return (0);
+}
