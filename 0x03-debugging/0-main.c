79322a395a145e1af631e4fb6a9082a66fb51837
Night4ury <a.waleedelzayat@gmail.com>
2023-07-20 13:55:56 -0700

    Adding

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,26 +1,6 @@
 #include "main.h"
 
 /**
- * positive_or_negative - Function Title
- *
- * Description: Loading...
- *
- * @i:number
- *
- * Return: nothing
- */
-
-void positive_or_negative(int i)
-{
-if (i > 0)
-printf("%d is positive.\n", i);
-else if (i < 0)
-printf("%d is negative.\n", i);
-else
-printf("The number is zero.\n");
-}
-
-/**
 * main - tests function that prints if integer is positive or negative
 * Return: 0
 */
