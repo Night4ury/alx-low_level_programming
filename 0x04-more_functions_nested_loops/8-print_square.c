34a7534ec9a515bc4fbca5782cc840d416789e8d
Night4ury <a.waleedelzayat@gmail.com>
2023-07-21 11:24:02 -0700

    Adding 7/21/2023

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -19,9 +19,10 @@ _putchar('\n');
 }
 else
 {
-for (int i = 0; i < size; i++)
+int i, j;
+for (i = 0; i < size; i++)
 {
-for (int j = 0; j < size; j++)
+for (j = 0; j < size; j++)
 _putchar('#');
 _putchar('\n');
 }
