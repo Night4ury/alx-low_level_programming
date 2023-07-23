b248edd72cc9da9ea29d42e0a863eac4daca221b
Night4ury <a.waleedelzayat@gmail.com>
2023-07-23 10:17:01 -0700

    Adding

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,39 +1,18 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
  * _abs - Islower to determine the char
  *
  * Description: Programm that prints the alphabet in lowercase.
  *
- * Return: nothing
- */
-int _abs(int num);
-
-/**
- * main - Start of the main fucntions
- *
- * Description: Programm that prints the alphabet in lowercase & uppercase.
+ * @num: paramter
  *
  * Return: nothing
  */
-
-int main(void)
-{
-int r;
-
-r = _abs(-1);
-printf("%d\n", r);
-r = _abs(0);
-printf("%d\n", r);
-r = _abs(1);
-printf("%d\n", r);
-r = _abs(-98);
-printf("%d\n", r);
-return (0);
-}
 
 int _abs(int num)
 {
 return (abs(num));
 }
-
