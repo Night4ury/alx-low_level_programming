8998f457f2c90b4e32723cf630cab0ef6757928d
Night4ury <a.waleedelzayat@gmail.com>
2023-07-23 09:52:40 -0700

    Adding

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -20,23 +20,3 @@ return (0);
 }
 }
 
-/**
- * main - Start of the main fucntions
- *
- * Description: Programm that prints the alphabet in lowercase & uppercase.
- *
- * Return: nothing
- */
-
-int main(void)
-{
-int r;
-r = _islower('H');
-putchar(r + '0');
-r = _islower('o');
-putchar(r + '0');
-r = _islower(108);
-putchar(r + '0');
-putchar('\n');
-return (0);
-}
