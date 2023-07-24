41c3806a8400c74d7ec7ce5878087e71945a19ed
Night4ury <a.waleedelzayat@gmail.com>
2023-07-24 10:42:35 -0700

    Adding 7/24/2023

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _puts - Function Title
+ *
+ * Description: Loading...
+ *
+ * @str: paramter
+ *
+ * Return: nothing
+ */
+
+void _puts(char *str)
+{
+puts(str);
+}
