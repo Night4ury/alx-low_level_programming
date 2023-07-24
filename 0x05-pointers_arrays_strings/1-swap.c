b1a099a9919bc0f8c446ded21cb9d24c4acbf686
Night4ury <a.waleedelzayat@gmail.com>
2023-07-24 09:56:26 -0700

    Adding 7/24/2023

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - Function Title
+ *
+ * Description: Loading...
+ *
+ * @a: paramter
+ * @b: paramter
+ *
+ * Return: nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+int tmp; 
+tmp = *a;
+*a = *b;
+*b = tmp;
+}
