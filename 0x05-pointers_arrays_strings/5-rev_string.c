9f843720679aa944f28c58d8f19b214f4ee2b712
Night4ury <a.waleedelzayat@gmail.com>
2023-07-24 11:47:17 -0700

    Adding 7/24/2023

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * rev_string - Function Title
+ *
+ * Description: Loading...
+ *
+ * @s: paramter
+ *
+ * Return: nothing
+ */
+
+void rev_string(char *s)
+{
+char *start = s;
+while (s != '\0')
+{
+s++;   
+}
+s--;
+while (start < s)
+{
+char temp = *start;
+*start = *s;
+*s = temp;
+start++;
+s--;
+}
+}
