bca3a02afc92c380fba84bf9ac0d3fb266bd9128
Night4ury <a.waleedelzayat@gmail.com>
2023-07-24 11:08:49 -0700

    Adding 7/24/2023

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _puts - Function Title
+ *
+ * Description: Loading...
+ *
+ * @s: paramter
+ *
+ * Return: nothing
+ */
+
+void print_rev(char *s)
+{
+char *p_to_s = s;
+while (*p_to_s != '\0')
+{
+*p_to_s++;
+}
+p_to_s--;
+while (p_to_s >= s)
+{
+putchar(*p_to_s);
+p_to_s--;
+}
+}
