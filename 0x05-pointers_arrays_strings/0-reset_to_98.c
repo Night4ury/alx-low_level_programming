32c72d35bcfaf06eafd29f82be1b3d6fb2a06c6c
Night4ury <a.waleedelzayat@gmail.com>
2023-07-24 09:50:12 -0700

    Adding 7/24/2023

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 -  Function Title
+ *
+ * Description: Loading...
+ *
+ * @n: paramter
+ *
+ * Return: nothing
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
