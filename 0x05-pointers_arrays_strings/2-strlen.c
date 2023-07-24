220e4d2a9dcf0df472021d76c9044d1790818913
Night4ury <a.waleedelzayat@gmail.com>
2023-07-24 10:03:37 -0700

    Adding 7/24/2023

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strlen - Function Title
+ *
+ * Description: Loading...
+ *
+ * @s: paramter
+ *
+ * Return: nothing
+ */
+
+int _strlen(char *s)
+{
+return (strlen(s));
+}
