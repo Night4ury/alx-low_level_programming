007fe5aac71e47357899ebf968580196b1591a2a
Night4ury <a.waleedelzayat@gmail.com>
2023-07-27 12:39:42 -0700

    Adding

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strcat - Function Title
+ *
+ * Description: Loading...
+ *
+ * @dest: paramter
+ * @src: paramter
+ *
+ * Return: nothing
+ */
+
+char *_strcat(char *dest, char *src)
+{
+char *s1 = dest;
+char *s2 = src;
+return (strcat(s1, s2));
+}
