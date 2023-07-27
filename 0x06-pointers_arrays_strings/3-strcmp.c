578bee68305f0eac365b8ff5d991a1b518524cb3
Night4ury <a.waleedelzayat@gmail.com>
2023-07-27 12:51:45 -0700

    Adding

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strcmp - Function Title
+ *
+ * Description: Loading...
+ *
+ * @s1: paramter
+ * @s2: paramter
+ *
+ * Return: nothing
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+return (strcmp(s1, s2));
+}
