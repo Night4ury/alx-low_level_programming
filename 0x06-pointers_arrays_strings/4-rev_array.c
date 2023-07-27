2885a3b5cf5ef4c3a2c1d4b28de5d353fd56ab21
Night4ury <a.waleedelzayat@gmail.com>
2023-07-27 13:36:25 -0700

    Adding

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * reverse_array - Function Title
+ *
+ * Description: Loading...
+ *
+ * @a: paramter
+ * @n: paramter
+ *
+ * Return: nothing
+ */
+
+void reverse_array(int *a, int n)
+{
+int *tmp = a;
+int tmp2 = 0;
+int i = 0;
+while (i <= (n - 1))
+{
+tmp++;
+i++;
+}
+i = 0;
+tmp--;
+while (i <= (n / 2))
+{
+tmp2 = *a;
+*a = *tmp;
+*tmp = tmp2;
+tmp--;
+a++;
+i++;
+}
+}
