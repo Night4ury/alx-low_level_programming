d53a93f2f25cc6850f3b12b814cf39384f839d55
Night4ury <a.waleedelzayat@gmail.com>
2023-07-31 11:18:05 -0700

    Adding

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+
+/**
+ * _strchr - Function Title
+ *
+ * Description: Loading...
+ *
+ * @s: paramter
+ * @c: paramter
+ *
+ * Return: nothing
+ */
+
+char *_strchr(char *s, char c)
+{
+return (strchr(s, c));
+}
