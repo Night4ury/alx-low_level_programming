e5e109c54f98be8793cdea70d4f0c521ca91715e
Night4ury <a.waleedelzayat@gmail.com>
2023-07-31 11:10:15 -0700

    Adding

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+
+/**
+ * _memcpy - Function Title
+ *
+ * Description: Loading...
+ *
+ * @dest: paramter
+ * @src: paramter
+ * @n: paramter
+ *
+ * Return: nothing
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+return (memset(s, b, n));
+}
