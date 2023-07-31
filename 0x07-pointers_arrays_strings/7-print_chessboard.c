915485beed4d13ffc715072ed2fa20812ba2544c
Night4ury <a.waleedelzayat@gmail.com>
2023-07-31 12:25:34 -0700

    Adding

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+
+/**
+ * print_chessboard - Function Title
+ *
+ * Description: Loading...
+ *
+ * @a: paramter
+ *
+ * Return: nothing
+ */
+
+void print_chessboard(char (*a)[8])
+{
+int i = 0, j = 0;
+while (i < 8){
+while (j < 8)
+{
+printf("%c", a[i][j]);
+j++;
+}
+printf("\n");  
+i++;
+j = 0;
+}
+}
