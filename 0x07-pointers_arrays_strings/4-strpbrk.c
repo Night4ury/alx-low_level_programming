aedca3d743798bf67c5d1836d0ab0a6138633391
Night4ury <a.waleedelzayat@gmail.com>
2023-07-31 11:28:21 -0700

    Adding

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+
+/**
+ * _strpbrk - Function Title
+ *
+ * Description: Loading...
+ *
+ * @s: paramter
+ * @accept: paramter
+ *
+ * Return: nothing
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+return (strpbrk(s, accept));
+}
