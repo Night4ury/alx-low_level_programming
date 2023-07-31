43ab38cf802cdb781036e79585c47fac4a10d57f
Night4ury <a.waleedelzayat@gmail.com>
2023-07-31 11:30:18 -0700

    Adding

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+
+/**
+ * _strstr - Function Title
+ *
+ * Description: Loading...
+ *
+ * @haystack: paramter
+ * @needle: paramter
+ *
+ * Return: nothing
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+return (strstr(haystack, needle));
+}
