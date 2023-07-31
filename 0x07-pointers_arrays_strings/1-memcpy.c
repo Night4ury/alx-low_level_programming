5fb496ede17b464315201350f869227b19f64529
Night4ury <a.waleedelzayat@gmail.com>
2023-07-31 11:03:45 -0700

    Adding

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+
+/**
+ * _memcpy - Function Title
+ *
+ * Description: Loading...
+ *
+ * @dest: paramter
+ * @src: paramter
+ * @n: paramter
+ *
+ * Return: nothing
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+return (memcpy(dest, src, n));
+}
