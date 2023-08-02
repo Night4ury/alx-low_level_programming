0f377c789b967b229eb8f39716a403ed6f456922
Night4ury <a.waleedelzayat@gmail.com>
2023-08-02 14:47:29 -0700

    Adding

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -18,8 +18,8 @@ int _strlen_recursion(char *s)
 int len = 0;
 if (*s == '\0')
 {
-    return(0);
+return (0);
 }
 len++;
-return(len + _strlen_recursion(s+1));
+return (len + _strlen_recursion(s + 1));
 }
