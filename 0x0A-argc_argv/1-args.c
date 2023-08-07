02be41f60c4b5e670278c6f125aed4060c8eb85e
Night4ury <a.waleedelzayat@gmail.com>
2023-08-07 12:20:45 -0700

    Adding

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Function Title
+ *
+ * Description: Loading...
+ *
+ * @argc: paramter
+ * @argv: paramter
+ *
+ * Return: nothing
+ */
+
+int main(int argc, char *argv[])
+{
+printf("%d\n", argc);
+return (0);
+}
