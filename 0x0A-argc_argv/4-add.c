9b06e93704d393e22db7eab24da95508fc4b8e03
Night4ury <a.waleedelzayat@gmail.com>
2023-08-09 23:53:26 +0300

    4-add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -16,31 +16,34 @@
 
 int main(int argc, char *argv[])
 {
-int i = 1;
-int sum = 0;
-char flag = 1;
-if (argc < 2)
-printf("%d\n", sum);
-else
-{
-    for(; i< argc; i++)
-    {
-        if (isalpha(atoi(argv[i])))
-        {
-            printf("flag = 0\n");
-            flag = 0;
-        }
-        else
-        {
-            printf("flag = 1\n");
-            sum += atoi(argv[i]);
-        }
-    }
 
-    if (flag == 1)
-    printf("%d\n", sum);
-    else
-    printf("Error\n");
-}
-return (0);
+	int i = 1;
+	int sum = 0;
+	char flag = 1;
+
+	if (argc < 2)
+	printf("%d\n", sum);
+
+	else
+	{
+		for(; i< argc; i++)
+		{
+			int temp = atoi(argv[i]);
+			if (temp == 0)
+			{
+				flag = 0;
+			}
+			else
+			{
+				sum += atoi(argv[i]);
+			}
+		}
+
+		if (flag == 1)
+		printf("%d\n", sum);
+		else
+		printf("Error\n");
+	}
+
+	return (0);
 }
