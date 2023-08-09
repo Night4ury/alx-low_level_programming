01b54a637eb21d082509e9a2481ac754e718837f
Night4ury <a.waleedelzayat@gmail.com>
2023-08-10 01:48:37 +0300

    Adding 1-strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - Function Title
+ *
+ * Description: Loading...
+ *
+ * @str: paramter
+ *
+ * Return: nothing
+ */
+
+char *_strdup(char *str)
+{
+    int size = 0; 
+    int i = 0;
+    char *ptr_tostr = str;
+    char *p;
+    
+
+    while (*ptr_tostr != '\0')
+    {
+        size++;
+        ptr_tostr++;
+    }
+
+    p = (char *)malloc(size);
+
+    if (p == NULL || size == 0)
+	{
+		return (NULL);
+	}
+
+    for(; i < size; i++)
+    {
+        p[i] = str[i];
+    }
+
+    return (p);
+}
