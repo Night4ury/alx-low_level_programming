90da5a6e6f97919fc75c306c384a4b9561d2d159
Night4ury <a.waleedelzayat@gmail.com>
2023-08-10 01:00:36 +0300

    Adding 0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * create_array - Function Title
+ *
+ * Description: Loading...
+ *
+ * @size: paramter
+ * @c: paramter
+ *
+ * Return: nothing
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i = 0;
+
+	char *p = (char *)malloc(size * sizeof(c));
+
+	if (p == NULL)
+	{
+		printf("Memory allocation failed\n");
+		return NULL;
+	}
+	
+	for(; i < size; i++)
+	{
+		p[i] = c;
+	}
+
+	return (p);
+}
