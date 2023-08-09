6d286aebe321f9b8ff9da193fa88429f45605a5f
Night4ury <a.waleedelzayat@gmail.com>
2023-08-09 21:08:01 +0300

    Adding

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -14,18 +14,17 @@
 
 int main(int argc, char *argv[])
 {
-    int mul = 0;
-    int num_1 = atoi(argv[1]);
-    int num_2 = atoi(argv[2]);
-    if (argc > 1)
-    {
-        
-        mul =  num_1 * num_2;
-        printf("%d\n", mul);
-    }
-    else
-    {
-        printf("Error");
-    }
-    return (0);
+int mul = 0;
+int num_1 = atoi(argv[1]);
+int num_2 = atoi(argv[2]);
+if (argc > 1)
+{
+mul =  num_1 * num_2;
+printf("%d\n", mul);
+}
+else
+{
+printf("Error");
+}
+return (0);
 }
