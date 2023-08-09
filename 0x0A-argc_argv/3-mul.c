0ec1583f244d816828022f7672779e5161c22f91
Night4ury <a.waleedelzayat@gmail.com>
2023-08-09 23:43:16 +0300

    mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,30 +2,34 @@
 #include <stdlib.h>
 
 /**
- 
-main - Function Title*
-Description: Loading...
-*
-@argc: parameter
-@argv: parameter
-*
-Return: nothing
-*/
+ * main - Function Title
+ *
+ * Description: Loading...
+ *
+ * @argc: parameter
+ * @argv: parameter
+ *
+ * Return: nothing
+ */
 
-int main(int argc, char argv[])
+int main(int argc, char *argv[])
 {
-int mul = 1;
-int num_1 = atoi(argv[1]);
-int num_2 = atoi(argv[2]);
-if (argc > 1)
-{
-mul =  num_1 * num_2;
-printf("%d\n", mul);
-}
-else
-{
-printf("Error");
-return (1);
-}
-return (0);
+	int mul = 0;
+	int num_1;
+	int num_2;
+
+	if (argc == 3)
+	{
+		num_1 = atoi(argv[1]);
+		num_2 = atoi(argv[2]);
+		mul =  num_1 * num_2;
+		printf("%d\n", mul);
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+
 }
