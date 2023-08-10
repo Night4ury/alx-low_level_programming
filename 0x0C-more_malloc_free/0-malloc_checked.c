1e18c3232f9e59fbd10fff1e815e717ff293c147
Night4ury <a.waleedelzayat@gmail.com>
2023-08-11 02:16:04 +0300

    Adding 0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - Function Title
+ *
+ * Description: Loading...
+ *
+ * @b: paramter
+ *
+ * Return: nothing
+ */
+
+void *malloc_checked(unsigned int b)
+{
+
+	int *p = (int *)malloc(b * sizeof(int));
+
+	if (p == NULL)
+	{
+		return (NULL);
+		exit(98);
+	}
+
+	return (p);
+}
+
