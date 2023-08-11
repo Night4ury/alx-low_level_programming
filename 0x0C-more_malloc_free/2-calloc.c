3f0ec6cc1610577d1fe097374bed78399294f9cc
Night4ury <a.waleedelzayat@gmail.com>
2023-08-11 03:02:34 +0300

    Adding 2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _calloc - Function Title
+ *
+ * Description: Loading...
+ *
+ * @s1: paramter
+ * @s2: paramter
+ * @n: paramter
+ *
+ * Return: nothing
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *p;
+
+	if (!nmemb || !size)
+		return (NULL);
+	p = malloc(nmemb * size);
+	if (!p)
+		return (NULL);
+	nmemb *= size;
+	while (nmemb--)
+		p[nmemb] = 0;
+	return (p);
+}
