10d8eb847af866677a8e69fe50ad841930a4bcf6
Night4ury <a.waleedelzayat@gmail.com>
2023-08-14 22:53:11 +0300

    Adding 1-init_dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,26 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * init_dog - Function Title
+ *
+ * Description: Loading...
+ *
+ * @d: paramter
+ * @name: paramter
+ * @age: paramter
+ * @owner: paramter
+ *
+ * Return: nothing
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d = malloc(sizeof(struct dog));
+	if (d == NULL)
+		return (NULL);
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
