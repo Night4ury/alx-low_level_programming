5e0b859f0bb68457f59debf256797fbb9e49cefe
Night4ury <a.waleedelzayat@gmail.com>
2023-08-16 05:54:12 +0300

    Adding

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Function Title
+ *
+ * Description: Loading...
+ *
+ * Return: nothing
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+    return (0);
+}
